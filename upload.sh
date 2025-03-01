7d1546b227a773cebcc19a867fe375befdd70625
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2025-03-01 19:19:34 +0800

    auto commit by win-upload.bat

---
+++ upload.sh
@@ -0,0 +1,6 @@
+echo '--------upload files start--------'   
+git add .
+git commit -m "auto update"
+echo '--------commit successfully--------'
+git push -u https://github.com/Shuaiwen-Cui/Research-Excitation_Structure_Response.git main
+echo '--------push to GitHub successfully--------'
